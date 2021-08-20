b6cad2eec9276b78719f018dac1e947eb166afe7
Alex DaRe <alexdare93@gmail.com>
2021-08-19 17:57:57 -0700

    added dependencies, started on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,38 @@
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_MATCHUPS } from '../utils/queries';
+import Button from '@material-ui/core/Button';
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_MATCHUPS, {
     fetchPolicy: "no-cache"
   });
 
+  const handleButtonClick = () => {
+    console.log("click")
+  };
+
   const matchupList = data?.matchups || [];
 
   return (
-    <div className="card bg-white card-rounded w-50">
-      <div className="card-header bg-dark text-center">
-        <h1>Welcome to Tech Matchup!</h1>
-      </div>
-      <div className="card-body m-5">
-        <h2>Here is a list of matchups you can vote on:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="square">
-            {matchupList.map((matchup) => {
-              return (
-                <li key={matchup._id}>
-                  <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                    {matchup.tech1} vs. {matchup.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="card-footer text-center m-3">
-        <h2>Ready to create a new matchup?</h2>
-        <Link to="/matchup">
-          <button className="btn btn-lg btn-danger">Create Matchup!</button>
-        </Link>
-      </div>
+    <div>
+      <container className="title">
+        <Col>
+        <h1>RoomUP</h1>
+        <h2>Find your next place</h2>
+        </Col>
+      </container>
+
+      <container>
+        <Row>
+          <Col className="search">
+          <input placeholder="insert text"></input>
+          <Button color="" variant="" onClick={ () => {handleButtonClick()}}>Search</Button>
+          </Col>
+        </Row>
+      </container>
+
+      
     </div>
   );
 };
