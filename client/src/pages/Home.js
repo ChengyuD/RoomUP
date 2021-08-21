fd6a30d20920d7b63738f1abfcd99a41ff0827c2
Alex DaRe <alexdare93@gmail.com>
2021-08-21 12:01:04 -0700

    added dropdown menu

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,19 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem'
+import { useState } from 'react'
 
 
 function Home() {
   const handleButtonClick = () => {
     console.log("click")
   };
+
+  const [searchValue, setSearchValue] = useState('');
+
+
   return (
    <body>
 
@@ -33,13 +40,31 @@ function Home() {
             <p>Where are you looking to live?</p>
           </Grid>
 
-          <Grid item md={7} xs={6}
+          <Grid item md={4} xs={6}
                 className="search"
                 container
                 direction="row"
                 justifyContent="center"
                 alignItems="stretch">
-            <TextField variant="outlined" placeholder="Irvine, CA" className="search-bar"/>
+            <TextField variant="filled" placeholder="Irvine, CA" className="search-bar"/>
+          </Grid>
+
+          <Grid item md={1} xs={6}
+                className="search"
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="stretch">
+            <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value="Type of housing"
+                  onChange={() => console.log("something")}
+                >
+                  <MenuItem value={"apartment"}>Apartment</MenuItem>
+                  <MenuItem value={"condo"}>Condo</MenuItem>
+                  <MenuItem value={"house"}>House</MenuItem>
+                </Select>
           </Grid>
 
           <Grid item md={2} 
@@ -48,7 +73,7 @@ function Home() {
                 direction="column"
                 justifyContent="center"
                 alignItems="center">
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" onClick={() => console.log("click")}>
                 Search
             </Button> 
           </Grid>
