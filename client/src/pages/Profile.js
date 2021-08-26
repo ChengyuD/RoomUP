74627fa37598da9666fadfa369b232cc92306fad
Quoc Ha <qhaa190@gmail.com>
2021-08-26 13:02:49 -0700

    update

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,4 +2,43 @@ import { useQuery, useMutation } from '@apollo/client';
 import { useParams, Link } from 'react-router-dom';
 import { CREATE_VOTE } from '../utils/mutations';
 import { QUERY_MATCHUPS } from '../utils/queries';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ShareIcon from "@material-ui/icons/Share";
+import { Avatar, IconButton, CardMedia } from "@material-ui/core";
 
+const UserCard = props => {
+    const { avatarUrl, title, subtitle, description, imageUrl } = props;
+    return (
+      <Card>
+        <CardHeader
+          avatar={<Avatar src={avatarUrl} />}
+          action={
+            <IconButton aria-label="settings">
+              <ShareIcon />
+            </IconButton>
+          }
+          title={title}
+          subheader={subtitle}
+        />
+        <CardMedia style={{ height: "150px" }} image={imageUrl} />
+        <CardContent>
+          <Typography variant="body2" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Add</Button>
+          <Button size="small">Message</Button>
+        </CardActions>
+      </Card>
+    );
+  };
+  
+  export default UserCard;
